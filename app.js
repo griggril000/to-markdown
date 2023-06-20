97a87f9228795b3777e62ee49775817a663fe361
Riley Grigg <griggriley@gmail.com>
2023-06-20 12:01:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const downloadMarkdown = (filename, text) => {
-  const element = document.createElement('a');
-  element.setAttribute('href', 'data:text/markdown;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-  element.style.display = 'none';
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
-}
+const downloadButton = document.querySelector('#download-button');
 
-const markdownText = `# My Markdown File
+downloadButton.addEventListener('click', () => {
+  const markdownText = `# My Markdown File
 This is some text in my markdown file.`;
 
-downloadMarkdown('myMarkdownFile.md', markdownText);
+  downloadMarkdown('myMarkdownFile.md', markdownText);
+});
