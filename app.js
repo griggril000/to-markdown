499fc339e8cc3d0012bbe4b46870e8bc13b27b4d
Riley Grigg <griggriley@gmail.com>
2023-06-20 12:29:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-function downloadMarkdown(filename, text) {
+function downloadMarkdown(filename, text, layout, title, author) {
+  const frontMatter = `---
+layout: ${layout}
+title: ${title}
+author: ${author}
+---\n`;
+  const markdownText = frontMatter + text;
   const element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(markdownText));
   element.setAttribute('download', filename);
   element.style.display = 'none';
   document.body.appendChild(element);
@@ -13,5 +19,8 @@ const markdownContent = document.querySelector('#markdown-content');
 
 downloadButton.addEventListener('click', () => {
   const markdownText = markdownContent.value;
-  downloadMarkdown('myMarkdownFile.md', markdownText);
+  const layout = 'post';
+  const title = 'My Markdown Post';
+  const author = 'Your Name';
+  downloadMarkdown('myMarkdownFile.md', markdownText, layout, title, author);
 });
