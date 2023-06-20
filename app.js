86f1de007523db3a6b331587cb3fea199b63401e
Riley Grigg <griggriley@gmail.com>
2023-06-20 12:15:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+function downloadMarkdown(filename, text) {
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
 const downloadButton = document.querySelector('#download-button');
 const markdownContent = document.querySelector('#markdown-content');
 
